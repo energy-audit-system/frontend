b7d66af29e4c2899264f702ad48086d9bec2ae25
xeenlight <vovabvd@gmail.com>
2025-12-19 18:35:45 +0500

    logo modal

--- energy-audit/src/components/Modal/Modal.jsx
+++ energy-audit/src/components/Modal/Modal.jsx
@@ -1,13 +1,13 @@
 import "./Modal.scss";
-
-export default function Modal({ isOpen, onClose, title, children }) {
+import img from "../../image/header_logo_down2.png"
+export default function Modal({ isOpen, onClose, children }) {
   if (!isOpen) return null;
 
   return (
     <div className="modal-overlay" onClick={onClose}>
       <div className="modal" onClick={(e) => e.stopPropagation()}>
         <div className="modal__header">
-          <h3>{title}</h3>
+          <img src={img} alt="benka logo" className="benka_logo_modal" />
           <button className="modal__close" onClick={onClose}>
             ✕
           </button>
